54b462b87c12dc3575604e1097700bef2d6c4363
virginiasacudato <virginiasacudato@gmail.com>
2022-09-07 10:01:20 -0300

    Nuevos casos de prueba y readme

--- src/PageObject/Pages/PlanificacionHoraria.py
+++ src/PageObject/Pages/PlanificacionHoraria.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from os.path import join, dirname
 from dotenv import load_dotenv
+import re
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -24,7 +25,8 @@ class PlanificacionHoraria:
         self.ele_jor = '.jornadaItem:nth-child(2) > .jornadaTexto'
         self.second_ele_jor = '.jornadaItem:nth-child(3) > .jornadaTexto'
         self.color_day_work = '.jornadaEmpleado:nth-child(3)'
-
+        self.save_btn = 'aplicarCambios'
+        self.select_day = '//*[@id="tablaEmpleadosYJornadas"]/tr[4]/td[4]'
         # //*[@id="tablaEmpleadosYJornadas"]/tr[4]/td[2]/span
         # //*[@id="tablaEmpleadosYJornadas"]/tr[6]/td[2]/span  --> TR VA DE 2 EN 2
 
@@ -52,35 +54,44 @@ class PlanificacionHoraria:
     def get_day_work(self):
         return self.driver.find_element(By.CSS_SELECTOR, self.color_day_work)
 
+    def save_btn_changes(self):
+        return self.driver.find_element(By.ID, self.save_btn)
+
     # Actions
     def plan_horaria(self):
         self.get_horarios().click()
         time.sleep(3)
         self.get_planificacion_horaria().click()
 
-    # Select employees
+    # TEST SUITE - JORNADAS
     def select_employees(self):
         def cuenta():
             cuenta.numero += 2
             return cuenta.numero
 
+        def replaceString(text):
+            # ABADI SABRINA GABRIEL (310)
+            return re.sub(r'\([^)]*\)', '', str(text))
+
         cuenta.numero = 0
+        global emp_selected
+        global replacestr
         emp_selected = []
 
         for i in range(8):
             emp = self.driver.find_element(By.XPATH,
                                            '//*[@id="tableBodyEmpleados"]/tr[' + str(cuenta()) + ']/td/div/label')
             emp.click()
-
             emp_selected.append(emp.text)
-            print(emp.text)
 
-            # //*[@id="tableBodyEmpleados"]/tr[3]/td/div/label
-            # //*[@id="tableBodyEmpleados"]/tr[2]/td/div/label
+        replacestr = [replaceString(s) for s in emp_selected]
+        # print(replacestr)
+
+        # //*[@id="tableBodyEmpleados"]/tr[3]/td/div/label
+        # //*[@id="tableBodyEmpleados"]/tr[2]/td/div/label
 
         time.sleep(3)
-        return emp_selected
-
+        # return emp_selected
 
     # Generate employees
     def generate(self):
@@ -90,15 +101,29 @@ class PlanificacionHoraria:
     # Check employees generated
 
     def check_emp_gen(self):
+        # Conserva unicamente el nombre en el array
+        def replace_string(text):
+            return re.sub(r'\nLegajo: \d*', "", str(text))
+
         empleados_obtenidos = []
         employee = self.driver.find_elements(By.XPATH, '//*[@id="tablaEmpleadosYJornadas"]/tr/td/span[@title]')
 
+        # Recorre la la lista de emp. generados y obtiene el atributo title
         for employees in employee:
             empleado = employees.get_attribute("title")
-            print(empleado)
-            empleados_obtenidos.append(empleado)
+            empleados_obtenidos.append(empleado)  # Almecena el valor obtenido en el array
 
-    # Case 2: Select an employee and modify full time. Check cell color change.
+        new_emp_obt = [replace_string(s) for s in empleados_obtenidos]
+        # print(new_emp_obt)
+
+        check = any(item in new_emp_obt for item in replacestr)
+
+        if check:
+            print("Se generaron algunos empleados seleccionados.")
+        else:
+            print("Ningun empleado con calendario asignado.")
+
+    # Case 2: Select an employee and modify full time. Check cambio de jornada.
     # xpath=//tbody[@id='tablaEmpleadosYJornadas']/tr[4]/td[2]/span --> Employee
     # xpath=//tbody[@id='tablaEmpleadosYJornadas']/tr/th[2] --> Table
 
@@ -114,7 +139,19 @@ class PlanificacionHoraria:
             self.get_second_ele_jor().click()
         else:
             self.get_ele_jor().click()
-            print("No coincide la jornada, primer elemento clic.")
+            # print("No coincide la jornada, primer elemento clic.")
+
+    def save_changes(self):
+        self.get_btn_gen().click()
+        # Podria verificar el texto del modal exitoso
+
+    # Case 3: Modify specific days
+    def mod_spec_days(self):
+        color_day = []
+        day_jornada = self.driver.find_elements(By.XPATH, '//*[@id="tablaEmpleadosYJornadas"]/tr/td/div')
+        # //*[@id="tablaEmpleadosYJornadas"]/tr[4]/td[6]
+        for days_jornada in day_jornada:
+            days_jornada.get_attribute("iddia")
 
     @staticmethod
     def get_base_url():
--- test/Scripts/test_login.py
+++ test/Scripts/test_login.py
@@ -14,14 +14,14 @@ load_dotenv(dotenv_path)
 USER = os.environ.get("USER")
 PASSWORD = os.environ.get("PASSWORD")
 
+
 # Run -->  python -m pytest
 # Run with messages console --> python -m pytest -s
 
 
-
 class Test_login(WebDriverSetup):
 
-    def test_login(self):
+    def test_a_login(self):
         driver = self.driver
         self.driver.get(HomePage.get_base_url())
         home_page = HomePage(driver)
@@ -29,20 +29,31 @@ class Test_login(WebDriverSetup):
         home_page.login("admin", "admin")
         home_page.click_login()
 
-    def test_select_employ(self):
+    def test_b_select_employ(self):
         global plan_horaria
         driver = self.driver
         self.driver.get(PlanificacionHoraria.get_base_url())
         plan_horaria = PlanificacionHoraria(driver)
-        self.test_login()
+        self.test_a_login()
         time.sleep(9)
         plan_horaria.plan_horaria()
         time.sleep(5)
         plan_horaria.select_employees()
         plan_horaria.generate()
+        plan_horaria.check_emp_gen()
+
+    def test_c_mod_jornada_com(self):
+        self.test_b_select_employ()
         plan_horaria.mod_fulltime_employee()
         time.sleep(8)
-        # plan_horaria.check_emp_gen() --> En construcción
+        plan_horaria.save_changes()
+
+    def test_d_mod_jor_parc(self):
+        self.test_b_select_employ()
+        plan_horaria.mod_spec_days()
+
+
+
 
 
 
